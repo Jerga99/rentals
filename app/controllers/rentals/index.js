ae957608a4ac576996169df022fa579715a164a0
Filip Jerga <filipjerga@Filips-MacBook-Pro.local>
2018-02-27 20:53:42 +0100

    remove redundandn code

--- app/controllers/rentals/index.js
+++ app/controllers/rentals/index.js
@@ -1,10 +1,4 @@
 import RentalsController from '../rentals'
 
 export default RentalsController.extend({
-  actions: {
-    submit(id) {
-      debugger;
-      this.transitionToRoute('rentals.show', id);
-    }
-  }
 });
